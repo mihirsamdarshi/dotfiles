933ec844150dd3d1cd2836b015a513f303681274
Mihir Samdarshi <msamdars@stanford.edu>
2023-02-08 22:46:38 -0800

    fix: config.fish had missing end

--- fish/config.fish
+++ fish/config.fish
@@ -13,25 +13,27 @@ if status is-interactive
     end
 
     starship init fish | source
-    pyenv init - | source
 
     # Shorthand for venv creation
     alias newvenv='python3 -m venv venv && source venv/bin/activate.fish && pip install --upgrade pip setuptools wheel'
+    
+    function gscatjq -d "Pipe the output of `gsutil cat` to jq"
+      set JSON_FILE $argv[1]
+      gsutil cat "$JSON_FILE" | jq $argv[2..-1]
+    end
 
-function gscatjq -d "Pipe the output of `gsutil cat` to jq"
-  set JSON_FILE $argv[1]
-  gsutil cat "$JSON_FILE" | jq $argv[2..-1]
+    function jqless -d "Pipe the output of jq to less"
+      set JSON_FILE $argv[1]
+     
+      set JQ_ARGS "."
+
+      if test (count $argv) -eq 2
+        set JQ_ARGS $argv[2]
+      end
+
+      jq --color-output "$JQ_ARGS" "$JSON_FILE" | less --RAW-CONTROL-CHARS
+    end
+
 end
 
-function jqless -d "Pipe the output of jq to less"
-  set JSON_FILE $argv[1]
- 
-  set JQ_ARGS "."
-
-  if test (count $argv) -eq 2
-    set JQ_ARGS $argv[2]
-  end
-
-  jq --color-output "$JQ_ARGS" "$JSON_FILE" | less --RAW-CONTROL-CHARS
-end
-
+pyenv init - | source
