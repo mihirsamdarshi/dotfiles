03084647dfef94257d3ed1a2506dfcacde560c7d
Mihir Samdarshi <msamdars@stanford.edu>
2023-04-20 03:54:43 -0700

    fix: add new fish setup files/temp aliases

--- setup.fish
+++ setup.fish
@@ -1,4 +1,6 @@
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
+alias --save fish "CC=$(brew unlink --dry-run gcc | grep "$(brew --prefix)/bin" | grep '\/gcc-\d\d') $(which nvim)"
+
 omf install
-omf reload
+
