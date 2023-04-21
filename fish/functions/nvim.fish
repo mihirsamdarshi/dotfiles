8c4091189eb1c3941c81671b09a58622476c1ea3
Mihir Samdarshi <msamdars@stanford.edu>
2023-04-20 23:47:48 -0700

    feat: remove nvim alias

--- fish/functions/nvim.fish
+++
@@ -1,4 +0,0 @@
-function nvim --wraps=/opt/homebrew/bin/nvim --wraps='CC= /opt/homebrew/bin/nvim' --wraps=CC=\(brew\ unlink\ --dry-run\ gcc\ \|\ grep\ /opt/homebrew/bin\ \|\ grep\ \'\\/gcc-\\d\\d\'\)\ /opt/homebrew/bin/nvim --wraps='CC=/opt/homebrew/bin/gcc-12 /opt/homebrew/bin/nvim' --description 'alias nvim CC=/opt/homebrew/bin/gcc-12 /opt/homebrew/bin/nvim'
-  CC=/opt/homebrew/bin/gcc-12 /opt/homebrew/bin/nvim $argv
-        
-end
--- setup.fish
+++ setup.fish
@@ -1,6 +1,10 @@
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-alias --save fish "CC=$(brew unlink --dry-run gcc | grep "$(brew --prefix)/bin" | grep '\/gcc-\d\d') $(which nvim)"
+if [ (uname -s) = "Darwin" ]
+    alias --save nvim "CC=$(brew unlink --dry-run gcc | grep "$(brew --prefix)/bin" | grep '\/gcc-\d\d') $(which nvim)"
+else
+    alias --save nvim nvim
+end
 
 omf install
 
